808f9aa58f849a6d888e7090f308b4e327f6a4ef
Gabriel Correa <gabacorrea@protonmail.com>
2024-12-17 02:48:13 -0500

    Add send mail shell script and a simple makefile

---
+++ IDS/run_ids.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#
+# Copyright (c) Gaba <gabriel.correasb@protonmail.com>
+#
+
+MAIL_TO="admin@example.com"
+SCRIPT_DIR="$(cd "$dirname "${BASH_SOURCE[0]}")" && pwd)"
+PYTHON_SCRIPT="${SCRIPT_DIR}/ids.py"
+
+OUTPUT=$("PYTHON_SCRIPT")
+
+if echo "$OUTPUT" | grep -q "Suspicious dctivity detected"; then
+    echo "$OUTPUT" | mail -s "IDS Alert: Suspicious SSH Activity" "$MAIL_TO"
+fi
